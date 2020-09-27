5293fa8a96ace03d83c2513e02bba268eda3d0a4
Adam La Morre <alamorre@gmail.com>
2020-09-27 15:38:24 -0400

    Updates

---
+++ precache-manifest.5c014f00abd256897aeddb32f56ef9ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5aaf642b1c6f9c97394ffa44b65952a6",
+    "url": "./index.html"
+  },
+  {
+    "revision": "245b5566f67fb06559e2",
+    "url": "./static/css/main.42cc8b22.chunk.css"
+  },
+  {
+    "revision": "72ee3a306c0c592fe241",
+    "url": "./static/js/2.a298a475.chunk.js"
+  },
+  {
+    "revision": "3e9fe304421a6de4d5448a9a52d993dd",
+    "url": "./static/js/2.a298a475.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "245b5566f67fb06559e2",
+    "url": "./static/js/main.ad19f29e.chunk.js"
+  },
+  {
+    "revision": "668a92368f200b78cbbb",
+    "url": "./static/js/runtime-main.88c6bc92.js"
+  }
+]);
